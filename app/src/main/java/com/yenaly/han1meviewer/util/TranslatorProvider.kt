85a1339c47ca2c098baeb66abe277f766857076b
Mohin <122416157+Mohin2295747@users.noreply.github.com>
2025-08-20 11:44:34 +0600

    Create TranslatorProvider.kt

---
+++ app/src/main/java/com/yenaly/han1meviewer/util/TranslatorProvider.kt
@@ -0,0 +1,8 @@
+package com.yenaly.han1meviewer.util
+
+import com.google.mlkit.nl.translate.Translator
+
+object TranslatorProvider {
+    var zhTranslator: Translator? = null
+    var jaTranslator: Translator? = null
+}
